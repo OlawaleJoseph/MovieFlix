367b15872d885f6698d15edc0f6ea98a6e0e7217
OlawaleJoseph <adedekojoseph@gmail.com>
2020-12-17 20:39:17 +0100

    filter reducer test

---
+++ src/__tests__/redux/filterReducer.test.js
@@ -0,0 +1,18 @@
+import { FILTER_BOOK } from '../../actions/type';
+import reducer from '../../reducers/filter';
+
+describe('Movie Reducer', () => {
+  test('Should return the default state', () => {
+    const state = reducer(undefined, {});
+
+    expect(state).toEqual('0');
+  });
+
+  test('Should return the new state with appropriate type', () => {
+    const payload = '2';
+    const state = reducer({}, { type: FILTER_BOOK, payload });
+
+    expect(state).not.toEqual({});
+    expect(state).toEqual(payload);
+  });
+});
