7bd0700877ca721517a3b560671749f99982b754
OlawaleJoseph <adedekojoseph@gmail.com>
2020-12-17 09:53:07 +0100

    create action to set single movie

--- src/actions/type.js
+++ src/actions/type.js
@@ -1,2 +1,3 @@
 export const FILTER_BOOK = 'FILTER_BOOK';
 export const GET_GENRES = 'GET_GENRE';
+export const SET_MOVIE = 'SET_MOVIE';
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Navbar from './NavBar';
 import HomePage from '../pages/HomePage';
+import VideoPage from '../pages/VideoPage';
 
 const App = () => (
-  <div className="App">
-    <Navbar />
-    <HomePage />
-  </div>
+  <Router>
+    <div className="App">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/movie" component={VideoPage} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
